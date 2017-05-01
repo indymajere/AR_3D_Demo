a723193f2599e5d320348ba4a13498b6038a98d8
Indy Hsiao <indymajere@gmail.com>
2017-05-02 01:15:56 +0800

    AR demo v1.0

---
+++ Assets/vbScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vbButtonObject;
+	private GameObject zombie;
+
+	// Use this for initialization
+	void Start () {
+
+		vbButtonObject = GameObject.Find ("actionButton");
+		zombie = GameObject.Find ("Zombie");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb) {
+		zombie.GetComponent<Animation> ().Play ();
+//		Debug.Log("Pressed...");
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
+		zombie.GetComponent<Animation> ().Stop ();
+	}
+
+}
